9c3b655b75e37c59d2c82083b6c770b44d9e4ea1
Mark Dicker <mark@markdicker.co.uk>
2024-06-27 11:05:08 +0100

    Stable release 1.0.3

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 const fs = require('fs');
- 
 
-exports.themeVersion = function () {
 
-    // Open file demo.txt in read mode
-    fs.open('style.css', 'r', function (err, f) {
-        if (err) {
-            return console.error(err);
-        }
-        console.log(f);
-        console.log("File opened!!");
+const version_regex = /^[ \t\/*#@]*Version:(.*)$/mi;
+
+// const version_number_regex = /\s*(?:\*\/|\?>).*/;
+const version_number_regex = /(?:(\d+)\.)?(?:(\d+)\.)?(?:(\d+)\.\d+)/;
+
+async function themeVersion( filename = 'style.css' ) {
+    
+    return new Promise((resolve, reject) => {
+        
+        // Open file and read version number
+        fs.readFile( filename, function (err, data) {
+            if (err) {
+                // console.error(err);
+                reject ( err );
+            }
+            else 
+            {
+                version = version_regex.exec( data );
+
+                if ( version === null )
+                    resolve( 'undefined' );
+
+                // console.log( version[1] )
+                version_number = version_number_regex.exec( version[1] );
+                
+                // console.log( version_number[0] );
+                
+                if ( version_number === null )
+                    resolve( 'undefined' );
+
+                resolve( version_number[0] );
+            }
+        });
     });
 
-    // console.log( "Theme Version" );
 }
+
+exports.getVersion = themeVersion;
